36173b9ad59f71e1b99e4e2ec306510e12b7fd9c
Eghosa Ordia <eghordia130@gmail.com>
2025-03-03 07:52:04 +0100

    project section init

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import NavBar from "./components/navbar";
 import Hero from "./components/hero";
+import Liquidex from "./components/liquidex";
 
 function App() {
   return (
-    <>
+    <div className="relative h-screen overflow-hidden">
       <NavBar />
       <Hero />
-    </>
+      <Liquidex />
+    </div>
   );
 }
 
---
+++ src/components/liquidex.tsx
@@ -0,0 +1,52 @@
+const Liquidex: React.FC = () => {
+  return (
+    <>
+      <section className="fixed top-16 w-full h-full font-futura text-white">
+        <div className="h-full bg-dark-1 px-4 py-8">
+          <div className="heading flex justify-between">
+            <div className="info w-1/2 grid gap-4">
+              <p className="text-sm">
+                <span className="text-white">01</span>
+                <span className="text-grey-2">/04</span>
+              </p>
+              <h2 className="text-3xl text-white font-futura-bold">
+                LiquiDex - Cryptocurrency Management & Trading System
+              </h2>
+              <p>
+                <ul className="flex gap-4">
+                  <li className="flex flex-col gap-1">
+                    <span className="text-grey-2">Project</span>
+                    <span className="text-sm">LiquiDex</span>
+                  </li>
+                  <li className="flex flex-col gap-1">
+                    <span className="text-grey-2">Role</span>
+                    <span className="text-sm">Developer</span>
+                  </li>
+                  <li className="flex flex-col gap-1">
+                    <span className="text-grey-2">Date</span>
+                    <span className="text-sm">2025</span>
+                  </li>
+                </ul>
+              </p>
+            </div>
+            <div className="tags text-[#727D73] mb-auto">
+              <ul className="flex flex-col gap-3 text-sm font-futura-bold">
+                <li className="project-li">
+                  <span>Artificial Intelligence</span>
+                </li>
+                <li className="project-li">
+                  <span>Web Development</span>
+                </li>
+                <li className="project-li">
+                  <span>Automation</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Liquidex;
