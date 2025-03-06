acc51c6d1ad55708332e4a186ed3c04d2ee50c63
Eghosa Ordia <eghordia130@gmail.com>
2025-03-06 02:01:09 +0100

    feat: add Sidebar component with heading and body content

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,16 @@
+const Sidebar = () => {
+  return (
+    <aside>
+      <div className="heading flex justify-between">
+        <span className="inline-block w-4 h-4 my-auto"></span>
+        <button className="my-auto">Close</button>
+      </div>
+
+      <div className="body">
+        I am a full-stack web developer turning ideas into clean, sleek modern
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
