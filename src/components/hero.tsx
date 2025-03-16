a9c2d1e2f5878789de321e6f33cbd4e430adfda4
Eghosa Ordia <eghordia130@gmail.com>
2025-03-16 14:08:26 +0100

    feat: improve Hero component styling and update button functionality

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -26,9 +26,7 @@ const Hero: React.FC = () => {
   return (
     <section className="relative min-h-screen font-proxima flex flex-col md:flex-row gap-24 px-8 md:px-16 py-10">
       <div className="text-center md:text-left md:w-3/5 flex flex-col gap-8 mt-16 md:mt-0">
-        <h1 className="text-5xl md:text-8xl font-bold">
-          I'm Eghosa Ordia.
-        </h1>
+        <h1 className="text-5xl md:text-8xl font-bold">I'm Eghosa Ordia.</h1>
         <p className="md:text-xl">
           I am a full-stack web developer turning ideas into clean, sleek modern
           websites and web apps — and I’m available for work.
@@ -40,7 +38,8 @@ const Hero: React.FC = () => {
               href={path}
               target="_blank"
               rel="noreferrer"
-              className="py-2 px-3 rounded-md bs-2 border border-dark-blue my-auto"
+              className="py-2 px-3 rounded-md bs-2 border border-dark-blue my-auto 
+              transition-all duration-300 ease-in-out hover:-translate-y-2 hover:translate-x-2 hover:bg-white hover:text-dark-3"
             >
               <i className={icon}></i>
             </a>
@@ -72,8 +71,11 @@ const Hero: React.FC = () => {
           </li>
           <li className="relative group border-b border-grey-3">
             <span className="absolute bottom-0 left-0 h-px bg-grey-2 transition-all duration-300 ease-in-out w-0 group-hover:w-full"></span>
-            <button className="w-full py-2 flex justify-between gap-2">
-              <span className="my-auto">DexBotPro</span>
+            <button
+              onClick={() => scrollToSection("bullsh-t")}
+              className="w-full py-2 flex justify-between gap-2"
+            >
+              <span className="my-auto">Bullsh-t</span>
               <i className="fa-solid fa-arrow-right my-auto text-sm -translate-x-2 opacity-0 group-hover:translate-x-0 group-hover:opacity-100 transition-all duration-300 ease-in-out"></i>
             </button>
           </li>
